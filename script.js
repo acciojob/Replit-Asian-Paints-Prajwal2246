92df285d025302a2b5fb48ea82e44781a76659c2
Prajwal2246 <janbandhuprajwal28@gmail.com>
2025-11-12 00:49:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const submitBtn=document.getElementById("submit-btn");
+submitBtn.addEventListener("click",()=>{
+     const inputNum=document.querySelector("#input-id").value;
+     const color=document.querySelector("#color-id").value;	
+
+	for(let i=1;i<=9;i++){
+		document.getElementById(i.toString()).style.backgroundColor='transparent';
+	}
+
+	if(inputNum>=1 && inputNum<=9 && color){
+		const targetDiv=document.getElementById(inputNum);
+		if(targetDiv){
+		  targetDiv.style.backgroundColor=color;	
+		}
+	}else{
+		alert('Please enter a valid ID (1-9) and a color.');
+	}
+	
+		
+})
+
+
