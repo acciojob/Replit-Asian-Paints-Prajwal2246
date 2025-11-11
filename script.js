a443bec4d031999740d4860c5f975636cc455f30
Prajwal2246 <janbandhuprajwal28@gmail.com>
2025-11-12 00:56:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const submitBtn=document.getElementById("submit-btn");
 submitBtn.addEventListener("click",()=>{
-     const inputNum=document.querySelector("#input-id").value;
+     const inputNum=document.querySelector("#block-id").value;
      const color=document.querySelector("#color-id").value;	
 
 	for(let i=1;i<=9;i++){
@@ -15,9 +15,10 @@ submitBtn.addEventListener("click",()=>{
 		}
 	}else{
 		alert('Please enter a valid ID (1-9) and a color.');
-	}
-	
-		
+	}		
 })
-
+const resetBtn = document.getElementById(reset_button);
+for(let i=1;i<=9;i++){
+	document.getElemntById(i.toString()).style.backgroundColor="transparent";
+}
 
